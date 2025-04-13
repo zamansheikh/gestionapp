7ec025c9cbfe7bfa9c097a35b6fc3a15f458419b
zamansheikh <zaman6545@gmail.com>
2025-04-13 08:42:53 +0600

    Bump version to 1.9.7+1 and update Android versionCode to 18

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 17
+        versionCode = 18
         versionName = flutter.versionName
     }
 
