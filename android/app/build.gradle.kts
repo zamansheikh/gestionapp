cabdd1a8914ddb122db0d7e63ff627c63ba05580
Zaman Sheikh <zaman6545@gmail.com>
2025-04-14 21:33:40 +0600

    Bump versionCode to 20 and update version to 1.9.9+1 in pubspec.yaml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 19
+        versionCode = 20
         versionName = flutter.versionName
     }
 
